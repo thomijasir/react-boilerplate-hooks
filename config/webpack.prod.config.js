d1b0cddb7b95e18cc5ef58f9b5ad50c6bceaf869
Thomi At Gojek <thomi.jasir@gojek.com>
2022-11-12 15:17:36 +0700

    chore(Webpack): remove unused

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,4 +1,3 @@
-const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
